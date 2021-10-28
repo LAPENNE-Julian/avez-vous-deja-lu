1a7cfbadf9ea093f077f348bb87d066aef765135
Benjamin Fourmont <fourmont.ben@gmail.com>
2021-10-28 11:57:46 +0200

    Creation of methods read, edit, add and delete for category

---
+++ avez-vous-deja-lu/src/Form/CategoryType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                "label" => "Name",
+            ])
+            ->add('color', TextType::class, [
+                "label" => "Color",
+            ])
+            ->add('img', null, [
+                "label" => "Image",
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class,
+        ]);
+    }
+}
